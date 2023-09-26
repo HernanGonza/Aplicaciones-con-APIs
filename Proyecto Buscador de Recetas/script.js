d36438bd4dafa56959f0af8ac0530d7e4567af91
Jesus Palomino <panijesus25@gmail.com>
2023-09-25 20:32:21 -0500

    cambio total de Proyecto Buscador de Recetas

--- Proyecto Buscador de Recetas/script.js
+++ Proyecto Buscador de Recetas/script.js
@@ -1,74 +1,60 @@
-const botonIngrediente = document.getElementById('boton-ingrediente');
-const listadoRecetas = document.getElementById('listado-recetas');
-const input = document.getElementById('ingrediente');
+const ingredienteInput = document.getElementById('ingrediente-input');
+const buscarButton = document.getElementById('buscar-button');
+const contenedorRecetas = document.querySelector('.recetas-grid'); // Cambiado a '.recetas-grid'
 
-async function pedir() {
-    const ingrediente = input.value;
-    input.value = '';
-    listadoRecetas.innerHTML = '';
-    try {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
-        const infoPedido = await response.json();
-        for (let i = 0; i < infoPedido.meals.length; i++) {
-            const receta = infoPedido.meals[i];
-            const mostrarPedido = document.createElement('div');
-            const botonReceta = document.createElement('button');
-            botonReceta.setAttribute('id', receta.idMeal);
-            botonReceta.textContent = receta.strMeal;
-            mostrarPedido.appendChild(botonReceta);
-            mostrarPedido.style.margin="10px";
-            mostrarPedido.style.textAlign="center";
-            listadoRecetas.appendChild(mostrarPedido);
-            botonReceta.addEventListener('click', funcionReceta);
-        }
-
-        
-
-} catch (error) {
-    console.log(error); "no se pudo pedir"
-
-}}
-
-async function funcionReceta (event) {
-    try {
-        const contenedor = document.getElementsByClassName('contenedor-receta');
-        if (contenedor) {
-            contenedor[0]?.remove();
-        }
-        const botonReceta = event.target.id;
-        console.log(botonReceta);
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${botonReceta}`);
-        const recetaRespuesta = await response.json();
-        const divReceta = document.createElement('div');
-        divReceta.classList.add('contenedor-receta');
-        divReceta.style.border = '1px solid black';
-        event.target.parentElement.appendChild(divReceta);
-        const tituloReceta = document.createElement('h2');
-        divReceta.appendChild(tituloReceta);
-        tituloReceta.textContent = recetaRespuesta.meals[0].strMeal;
-        const instrucciones = document.createElement('p');
-        instrucciones.textContent = recetaRespuesta.meals[0].strInstructions;
-        console.log(recetaRespuesta.meals[0].strInstructions)
-        divReceta.appendChild(instrucciones);
-        const ingredientes = document.createElement('ul');
-        divReceta.appendChild(ingredientes);
-        for (let i = 1; i < 20; i++) {
-            const ingrediente = document.createElement('li');
-            ingrediente.style.listStyle = 'none';
-            const ingredientesParaMostrar = recetaRespuesta.meals[0][`strIngredient${i}`] + ' ' + recetaRespuesta.meals[0][`strMeasure${i}`];
-            if (ingredientesParaMostrar) {
-                ingrediente.textContent = ingredientesParaMostrar;
-                ingredientes.appendChild(ingrediente);
-            }
-            
-        }
-
-
+buscarButton.addEventListener('click', () => {
+    const ingrediente = ingredienteInput.value.trim();
+    if (ingrediente !== '') {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`)
+            .then(response => response.json())
+            .then(data => {
+                contenedorRecetas.innerHTML = '';
+                if (data.meals) {
+                    data.meals.forEach(meal => {
+                        obtenerDetallesReceta(meal.idMeal);
+                    });
+                } else {
+                    contenedorRecetas.innerHTML = '<p>No se encontraron recetas con este ingrediente.</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
     }
-    catch (error) {
-        console.log(error);
-    }
+});
+
+function obtenerDetallesReceta(idReceta) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idReceta}`)
+        .then(response => response.json())
+        .then(data => {
+            const receta = data.meals[0];
+            mostrarReceta(receta);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
+function mostrarReceta(receta) {
+    const recetaDiv = document.createElement('div');
+    recetaDiv.classList.add('receta');
+    recetaDiv.innerHTML = `
+        <h2>${receta.strMeal}</h2>
+        <p><strong>Ingredientes:</strong> ${obtenerIngredientes(receta)}</p>
+        <p><strong>Instrucciones:</strong> ${receta.strInstructions}</p>
+        <img src="${receta.strMealThumb}">
+    `;
+    contenedorRecetas.appendChild(recetaDiv);
+}
 
-botonIngrediente.addEventListener('click', pedir);
+function obtenerIngredientes(receta) {
+    let ingredientes = [];
+    for (let i = 1; i <= 20; i++) {
+        const ingrediente = receta[`strIngredient${i}`];
+        const medida = receta[`strMeasure${i}`];
+        if (ingrediente && medida) {
+            ingredientes.push(`${medida} ${ingrediente}`);
+        }
+    }
+    return ingredientes.join(', ');
+}
